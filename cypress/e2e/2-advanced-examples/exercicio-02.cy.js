a0ee8d8ffe7a89e517c1980cbea6e94e196505c4
Vivokas <vivianevinici@gmail.com>
2025-10-18 14:34:31 -0300

    Ajuste time out

--- cypress/e2e/2-advanced-examples/exercicio-02.cy.js
+++ cypress/e2e/2-advanced-examples/exercicio-02.cy.js
@@ -1,88 +1,91 @@
-describe("Cadastrar entradas e saídas com bugs", { defaultCommandTimeout: 10000 }, () => {
+describe(
+  "Cadastrar entradas e saídas com bugs",
+  { defaultCommandTimeout: 9000000 },
+  () => {
+    it("Cadastrar uma nova transação de entrada - falha 1", () => {
+      //arrange, act, assert
 
-  it("Cadastrar uma nova transação de entrada - falha 1", () => {
-    //arrange, act, assert
-
-    cy.visit("https://devfinance-agilizei.netlify.app");
-
-    cy.contains("Nova Transação").click();
-    cy.get("#description").type("Mesada");
-    cy.get("#amount").type(100);
-    cy.get("#date").type("2023-02-01");
-
-    cy.contains("Salvar").click();
-
-    cy.get("tbody tr").should("have.length", 1);
-  });
-
-  it("Cadastrar uma nova transação de entrada - falha 2", () => {
-    cy.visit("https://devfinance-agilizei.netlify.app");
-
-    cy.contains("Nova Transação").click();
-    cy.get("#description").type("Mesada");
-    cy.get("#amount").type(100);
-    cy.get("#date").type("2023-02-01");
-
-    cy.contains("Salvar").click();
-
-    cy.get("tbody tr").should("have.length", 1);
-  });
-
-  it(
-    "Cadastrar uma nova transação de entrada - falha 3",
-    { defaultCommandTimeout: 10000 },
-    () => {
       cy.visit("https://devfinance-agilizei.netlify.app");
 
       cy.contains("Nova Transação").click();
       cy.get("#description").type("Mesada");
       cy.get("#amount").type(100);
-
       cy.get("#date").type("2023-02-01");
 
       cy.contains("Salvar").click();
 
       cy.get("tbody tr").should("have.length", 1);
-    }
-  );
+    });
 
-  //PAROU AQUI
-  it("Cadastrar uma nova transação de entrada - falha 4", () => {
-    cy.visit("https://devfinance-agilizei.netlify.app");
+    it("Cadastrar uma nova transação de entrada - falha 2", () => {
+      cy.visit("https://devfinance-agilizei.netlify.app");
 
-    cy.contains("Nova Transação").click();
+      cy.contains("Nova Transação").click();
+      cy.get("#description").type("Mesada");
+      cy.get("#amount").type(100);
+      cy.get("#date").type("2023-02-01");
 
-    cy.get("#amount").type(100);
-    cy.get("#description").type("Mesada");
-    cy.get("#date").type("2023-02-01");
-    cy.contains("Salvar").click();
+      cy.contains("Salvar").click();
 
-    cy.get("tbody tr").should("have.length", 1);
-  });
+      cy.get("tbody tr").should("have.length", 1);
+    });
 
-  it("Cadastrar uma nova transação de entrada - falha 5", () => {
-    cy.visit("https://devfinance-agilizei.netlify.app");
+    it(
+      "Cadastrar uma nova transação de entrada - falha 3",
+      { defaultCommandTimeout: 10000 },
+      () => {
+        cy.visit("https://devfinance-agilizei.netlify.app");
 
-    cy.contains("Nova Transação").click();
-    cy.get("#description").type("Mesada");
-    cy.get("#amount").type(100);
-    cy.get("#date").type("2023-02-01");
+        cy.contains("Nova Transação").click();
+        cy.get("#description").type("Mesada");
+        cy.get("#amount").type(100);
 
-    cy.contains("Salvar").click();
+        cy.get("#date").type("2023-02-01");
 
-    //cy.get(".alert").should("not.exist");
-  });
+        cy.contains("Salvar").click();
 
-  it("Cadastrar uma nova transação de entrada - falha 6", () => {
-    cy.visit("https://devfinance-agilizei.netlify.app");
+        cy.get("tbody tr").should("have.length", 1);
+      }
+    );
 
-    cy.contains("Nova Transação").click();
-    cy.get("#description").type("Mesada");
-    cy.get("#amount").type(100);
-    cy.get("#date").type("2023-02-01");
+    //PAROU AQUI
+    it("Cadastrar uma nova transação de entrada - falha 4", () => {
+      cy.visit("https://devfinance-agilizei.netlify.app");
 
-    cy.contains("Salvar").click();
+      cy.contains("Nova Transação").click();
 
-    cy.get("tbody tr").should("have.length", 1);
-  });
-});
+      cy.get("#amount").type(100);
+      cy.get("#description").type("Mesada");
+      cy.get("#date").type("2023-02-01");
+      cy.contains("Salvar").click();
+
+      cy.get("tbody tr").should("have.length", 1);
+    });
+
+    it("Cadastrar uma nova transação de entrada - falha 5", () => {
+      cy.visit("https://devfinance-agilizei.netlify.app");
+
+      cy.contains("Nova Transação").click();
+      cy.get("#description").type("Mesada");
+      cy.get("#amount").type(100);
+      cy.get("#date").type("2023-02-01");
+
+      cy.contains("Salvar").click();
+
+      //cy.get(".alert").should("not.exist");
+    });
+
+    it("Cadastrar uma nova transação de entrada - falha 6", () => {
+      cy.visit("https://devfinance-agilizei.netlify.app");
+
+      cy.contains("Nova Transação").click();
+      cy.get("#description").type("Mesada");
+      cy.get("#amount").type(100);
+      cy.get("#date").type("2023-02-01");
+
+      cy.contains("Salvar").click();
+
+      cy.get("tbody tr").should("have.length", 1);
+    });
+  }
+);
