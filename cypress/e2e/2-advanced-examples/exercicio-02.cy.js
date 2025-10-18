d04f1ebe44a4876feceebef4ae5746c186788809
Vivokas <vivianevinici@gmail.com>
2025-10-18 11:12:02 -0300

    Subindo correção testes e arquivo CI-CD

--- cypress/e2e/2-advanced-examples/exercicio-02.cy.js
+++ cypress/e2e/2-advanced-examples/exercicio-02.cy.js
@@ -27,25 +27,28 @@ describe("Cadastrar entradas e saídas com bugs", () => {
     cy.get("tbody tr").should("have.length", 1);
   });
 
-  it("Cadastrar uma nova transação de entrada - falha 3", () => {
-    cy.visit("https://devfinance-agilizei.netlify.app");
+  it(
+    "Cadastrar uma nova transação de entrada - falha 3",
+    { defaultCommandTimeout: 10000 },
+    () => {
+      cy.visit("https://devfinance-agilizei.netlify.app");
 
-    cy.contains("Nova Transação").click();
-    cy.get("#description").type("Mesada");
-    cy.get("#amount").type(100);
+      cy.contains("Nova Transação").click();
+      cy.get("#description").type("Mesada");
+      cy.get("#amount").type(100);
 
-    cy.get("#date").type("2023-02-01");
+      cy.get("#date").type("2023-02-01");
 
-    cy.contains("Salvar").click();
+      cy.contains("Salvar").click();
 
-    cy.get("tbody tr").should("have.length", 1);
-  });
+      cy.get("tbody tr").should("have.length", 1);
+    }
+  );
 
   //PAROU AQUI
   it("Cadastrar uma nova transação de entrada - falha 4", () => {
     cy.visit("https://devfinance-agilizei.netlify.app");
 
-    
     cy.contains("Nova Transação").click();
 
     cy.get("#amount").type(100);
@@ -54,7 +57,6 @@ describe("Cadastrar entradas e saídas com bugs", () => {
     cy.contains("Salvar").click();
 
     cy.get("tbody tr").should("have.length", 1);
-
   });
 
   it("Cadastrar uma nova transação de entrada - falha 5", () => {
@@ -68,7 +70,6 @@ describe("Cadastrar entradas e saídas com bugs", () => {
     cy.contains("Salvar").click();
 
     //cy.get(".alert").should("not.exist");
-
   });
 
   it("Cadastrar uma nova transação de entrada - falha 6", () => {
